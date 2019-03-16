fc049745c5611b5f20275a2b4d8f3b3bc286819c
alvaro <alvar@DESKTOP-R9HFQUP>
2019-03-16 13:24:25 -0400

    Agregados formularios de crear usuario y producto Traduccion de DataTables al espanol

--- Inventario/js/demo/datatables-demo.js
+++ Inventario/js/demo/datatables-demo.js
@@ -1,4 +1,31 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+        "language": {
+            "oAria": {
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            },
+            "oPaginate": {
+                "sFirst":    "Primera pagina",
+                "sLast":     "Última pagina",
+                "sNext":     "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "emptyTable":"No hay nada que mostrar",
+            "info":"Mostrando del _START_ al _END_ de _TOTAL_",
+            "infoEmpty":"No hay registros",
+            "infoFiltered":"(filtrado de un total de _MAX_)",
+            "infoPostFix":"",
+            "decimal":",",
+            "tousands":".",
+            "lengthMenu":"Mostrar _MENU_ registros",
+            "loadingRecords":"Cargando...",
+            "processing":"Procesando...",
+            "search":"Buscar:",
+            "searchPlaceholder":"",
+            "url":"",
+            "zeroRecords":"No hay coincidencias"
+        }
+    });
 });
