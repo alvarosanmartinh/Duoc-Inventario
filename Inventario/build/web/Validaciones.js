fede47a1d732d2988eb7a5f735cb1c365f355f00
alvarosmh <alvaro.sanmartinh@gmail.com>
2019-03-23 14:59:49 -0300

    creacion jsp
    Proyecto JSP

---
+++ Inventario/build/web/Validaciones.js
@@ -0,0 +1,10 @@
+function phonenumber(numero)
+{
+    var formato = /^\+?([0-9]{3})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
+    if(numero.value.match(formato)){        
+        return true;
+    }else{        
+        alert("Por favor ingresar número en formato + xxx xxxx xxxx");
+        return false;
+    }
+}
