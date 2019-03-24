08d9f60015d52d303adc70a5eca973afc6a4f26c
alvarosmh <alvaro.sanmartinh@gmail.com>
2019-03-23 21:06:07 -0400

    Correcciones

--- Inventario/web/private/jsCT/validaciones.js
+++ Inventario/web/private/jsCT/validaciones.js
@@ -4,21 +4,38 @@
 jQuery('#logIn').click(function(){
     var user = jQuery('#user').val();
     var pass = jQuery('#pass').val();
-    if (!validaVacio(user.trim()) && !validaVacio(pass.trim())){
-        alert('Todo Ok');
-    }  else {
+    if (!user || !pass){
         alert('Complete todos los datos del formulario');
     }
 });
 
 
-function validaVacio(dato){
-    var traeVacio;
-    if (dato == null || dato == "" ){
-        traeVacio = true;
-    }else{
-        traeVacio = false;
+$(function(){
+
+  $("#contact-form").validate({
+    rules:{
+      nombres:{required:true, minlength:4, maxlength:40,lettersonly:true},
+      apellidos:{required:true, minlength:4, maxlength:40,lettersonly:true},
+      email:{required:true, email:true},
+      phone:{required:true,minlength:6, maxlength:40},
+      message:{required:true, minlength:4, maxlength:40}
+    },
+    messages:{
+      nombres:{
+        required:"Ingresar Nombres",lettersonly:"Solo ingrese Letras"}
     }
-    return traeVacio;
+  });
+
+});
+
+function phonenumber(numero)
+{
+    var formato = /^\+?([0-9]{3})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
+    if(numero.value.match(formato)){        
+        return true;
+    }else{        
+        alert("Por favor ingresar número en formato + xxx xxxx xxxx");
+        return false;
+    }
 }
 
