cd5a75e16024d955531c37c624f275ba2553b287
alvarosmh <alvaro.sanmartinh@gmail.com>
2019-04-01 16:04:37 -0300

    Falta corregir bug de paginas publicas y crear filtro de seguridad
    Creando zona privada y publica del sitio a traves del login, falta corregir el bug de about y contact y crear el filtro de jsf para que no deje entrar en las paginas que estan en el directorio private/ si no ha hecho login y que si deje entrar a index,about y contact en cualquier caso

--- Inventario/src/main/java/cl/duoc/inventario/infra/security/LogonMB.java
+++ Inventario/src/main/java/cl/duoc/inventario/infra/security/LogonMB.java
@@ -1,16 +1,17 @@
 package cl.duoc.inventario.infra.security;
 
+import static cl.duoc.inventario.util.Utils.addDetailMessage;
+import com.github.adminfaces.template.session.AdminSession;
 import org.omnifaces.util.Faces;
 
 import javax.enterprise.context.SessionScoped;
 import javax.enterprise.inject.Specializes;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import java.io.IOException;
 import java.io.Serializable;
 
-import static cl.duoc.inventario.util.Utils.addDetailMessage;
-import com.github.adminfaces.template.session.AdminSession;
+import com.github.adminfaces.template.config.AdminConfig;
+import javax.inject.Inject;
 
 /**
  * Created by rmpestano on 12/20/14.
@@ -29,16 +30,18 @@ import com.github.adminfaces.template.session.AdminSession;
 public class LogonMB extends AdminSession implements Serializable {
 
     private String currentUser;
-    private String user;
-    private String clave;
-    private boolean recordar;
+    private String email;
+    private String password;
+    private boolean remember;
+    @Inject
+    private AdminConfig adminConfig;
 
 
     public void login() throws IOException {
-        currentUser = user;
-        addDetailMessage("Has iniciado sesion como <b>" + user + "</b>");
+        currentUser = email;
+        addDetailMessage("Logged in successfully as <b>" + email + "</b>");
         Faces.getExternalContext().getFlash().setKeepMessages(true);
-        Faces.redirect("index.xhtml");
+        Faces.redirect(adminConfig.getIndexPage());
     }
 
     @Override
@@ -46,36 +49,30 @@ public class LogonMB extends AdminSession implements Serializable {
 
         return currentUser != null;
     }
-    
-    public void logout() throws IOException {
-        currentUser = null;
-        Faces.redirect("login.xhtml");
-    } 
 
-    public String getUser() {
-        return user;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUser(String user) {
-        this.user = user;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public String getClave() {
-        return clave;
+    public String getPassword() {
+        return password;
     }
 
-    public void setClave(String clave) {
-        this.clave = clave;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public boolean isRecordar() {
-        return recordar;
+    public boolean isRemember() {
+        return remember;
     }
 
-    public void setRecordar(boolean recordar) {
-        this.recordar = recordar;
+    public void setRemember(boolean remember) {
+        this.remember = remember;
     }
-
 
     public String getCurrentUser() {
         return currentUser;
