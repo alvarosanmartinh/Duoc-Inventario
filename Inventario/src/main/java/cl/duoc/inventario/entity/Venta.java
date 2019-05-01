25fa668fabb5d87b167517d7cf53788fb4dddee6
jgcordobaloyola <38819325+jgcordobaloyola@users.noreply.github.com>
2019-05-01 16:04:26 -0400

    Create Venta.java

---
+++ Inventario/src/main/java/cl/duoc/inventario/entity/Venta.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.duoc.inventario.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author ghost-pc
+ */
+@Entity
+@Table(name = "venta", catalog = "inventario", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Venta.findAll", query = "SELECT v FROM Venta v"),
+    @NamedQuery(name = "Venta.findByIdVenta", query = "SELECT v FROM Venta v WHERE v.idVenta = :idVenta"),
+    @NamedQuery(name = "Venta.findByFecha", query = "SELECT v FROM Venta v WHERE v.fecha = :fecha"),
+    @NamedQuery(name = "Venta.findByEliminado", query = "SELECT v FROM Venta v WHERE v.eliminado = :eliminado")})
+public class Venta implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "IdVenta")
+    private Integer idVenta;
+    @Basic(optional = false)
+    @Column(name = "fecha")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Basic(optional = false)
+    @Column(name = "eliminado")
+    private String eliminado;
+    @JoinColumn(name = "IdVenta", referencedColumnName = "IdDetVenta", insertable = false, updatable = false)
+    @OneToOne(optional = false)
+    private Detalleventa detalleventa;
+    @JoinColumn(name = "RutCliente", referencedColumnName = "Rut")
+    @ManyToOne(optional = false)
+    private Usuario rutCliente;
+
+    public Venta() {
+    }
+
+    public Venta(Integer idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Venta(Integer idVenta, Date fecha, String eliminado) {
+        this.idVenta = idVenta;
+        this.fecha = fecha;
+        this.eliminado = eliminado;
+    }
+
+    public Integer getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(Integer idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(String eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    public Detalleventa getDetalleventa() {
+        return detalleventa;
+    }
+
+    public void setDetalleventa(Detalleventa detalleventa) {
+        this.detalleventa = detalleventa;
+    }
+
+    public Usuario getRutCliente() {
+        return rutCliente;
+    }
+
+    public void setRutCliente(Usuario rutCliente) {
+        this.rutCliente = rutCliente;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idVenta != null ? idVenta.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Venta)) {
+            return false;
+        }
+        Venta other = (Venta) object;
+        if ((this.idVenta == null && other.idVenta != null) || (this.idVenta != null && !this.idVenta.equals(other.idVenta))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cl.duoc.inventario.entity.Venta[ idVenta=" + idVenta + " ]";
+    }
+    
+}
