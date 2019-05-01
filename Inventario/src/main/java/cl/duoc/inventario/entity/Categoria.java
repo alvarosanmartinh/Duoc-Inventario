94e5be4872f079ed7b0a31fa3c4e374603a38805
jgcordobaloyola <38819325+jgcordobaloyola@users.noreply.github.com>
2019-05-01 16:04:57 -0400

    Create Categoria.java

---
+++ Inventario/src/main/java/cl/duoc/inventario/entity/Categoria.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.duoc.inventario.entity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author ghost-pc
+ */
+@Entity
+@Table(name = "categoria", catalog = "inventario", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Categoria.findAll", query = "SELECT c FROM Categoria c"),
+    @NamedQuery(name = "Categoria.findByIdCategoria", query = "SELECT c FROM Categoria c WHERE c.idCategoria = :idCategoria"),
+    @NamedQuery(name = "Categoria.findByNombre", query = "SELECT c FROM Categoria c WHERE c.nombre = :nombre"),
+    @NamedQuery(name = "Categoria.findByDescripcion", query = "SELECT c FROM Categoria c WHERE c.descripcion = :descripcion"),
+    @NamedQuery(name = "Categoria.findByEliminado", query = "SELECT c FROM Categoria c WHERE c.eliminado = :eliminado")})
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "IdCategoria")
+    private Integer idCategoria;
+    @Basic(optional = false)
+    @Column(name = "Nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @Column(name = "Descripcion")
+    private String descripcion;
+    @Basic(optional = false)
+    @Column(name = "Eliminado")
+    private String eliminado;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "categoria")
+    private Collection<Subcategoria> subcategoriaCollection;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public Categoria(Integer idCategoria, String nombre, String descripcion, String eliminado) {
+        this.idCategoria = idCategoria;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.eliminado = eliminado;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(String eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    @XmlTransient
+    public Collection<Subcategoria> getSubcategoriaCollection() {
+        return subcategoriaCollection;
+    }
+
+    public void setSubcategoriaCollection(Collection<Subcategoria> subcategoriaCollection) {
+        this.subcategoriaCollection = subcategoriaCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idCategoria != null ? idCategoria.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Categoria)) {
+            return false;
+        }
+        Categoria other = (Categoria) object;
+        if ((this.idCategoria == null && other.idCategoria != null) || (this.idCategoria != null && !this.idCategoria.equals(other.idCategoria))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cl.duoc.inventario.entity.Categoria[ idCategoria=" + idCategoria + " ]";
+    }
+    
+}
