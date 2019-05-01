a189e19847dd1209538c54cfec36e2ae3184cafa
jgcordobaloyola <38819325+jgcordobaloyola@users.noreply.github.com>
2019-05-01 16:04:53 -0400

    Create Contacto.java

---
+++ Inventario/src/main/java/cl/duoc/inventario/entity/Contacto.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.duoc.inventario.entity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author ghost-pc
+ */
+@Entity
+@Table(name = "contacto", catalog = "inventario", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Contacto.findAll", query = "SELECT c FROM Contacto c"),
+    @NamedQuery(name = "Contacto.findByEmail", query = "SELECT c FROM Contacto c WHERE c.email = :email"),
+    @NamedQuery(name = "Contacto.findByNombre", query = "SELECT c FROM Contacto c WHERE c.nombre = :nombre"),
+    @NamedQuery(name = "Contacto.findByTelefono", query = "SELECT c FROM Contacto c WHERE c.telefono = :telefono"),
+    @NamedQuery(name = "Contacto.findByMotivo", query = "SELECT c FROM Contacto c WHERE c.motivo = :motivo"),
+    @NamedQuery(name = "Contacto.findByRequerimiento", query = "SELECT c FROM Contacto c WHERE c.requerimiento = :requerimiento")})
+public class Contacto implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @Column(name = "nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @Column(name = "telefono")
+    private String telefono;
+    @Basic(optional = false)
+    @Column(name = "Motivo")
+    private String motivo;
+    @Basic(optional = false)
+    @Column(name = "requerimiento")
+    private String requerimiento;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "email")
+    private Collection<Usuario> usuarioCollection;
+
+    public Contacto() {
+    }
+
+    public Contacto(String email) {
+        this.email = email;
+    }
+
+    public Contacto(String email, String nombre, String telefono, String motivo, String requerimiento) {
+        this.email = email;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.motivo = motivo;
+        this.requerimiento = requerimiento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public String getRequerimiento() {
+        return requerimiento;
+    }
+
+    public void setRequerimiento(String requerimiento) {
+        this.requerimiento = requerimiento;
+    }
+
+    @XmlTransient
+    public Collection<Usuario> getUsuarioCollection() {
+        return usuarioCollection;
+    }
+
+    public void setUsuarioCollection(Collection<Usuario> usuarioCollection) {
+        this.usuarioCollection = usuarioCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (email != null ? email.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Contacto)) {
+            return false;
+        }
+        Contacto other = (Contacto) object;
+        if ((this.email == null && other.email != null) || (this.email != null && !this.email.equals(other.email))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cl.duoc.inventario.entity.Contacto[ email=" + email + " ]";
+    }
+    
+}
